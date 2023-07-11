f94d08b543ff1be9bfca34e9cee9325075439bf9
Oleksandr Patuk <oleksandr.patuk@gmail.com>
2023-07-11 21:56:20 +0300

    Add AWS IAM policy file with required permissions to the repo

---
+++ spot_optimizer-policy.js
@@ -0,0 +1,17 @@
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeSpotPriceHistory",
+                "ec2:GetInstanceTypesFromInstanceRequirements",
+                "ec2:DescribeInstances",
+                "ec2:DescribeInstanceTypes",
+                "pricing:GetProducts"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
