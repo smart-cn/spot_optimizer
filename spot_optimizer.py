cb3cd0c38d7927822b0e0216eea3953ecfbc6d47
Oleksandr Patuk <oleksandr.patuk@gmail.com>
2023-07-17 22:24:38 +0300

    Implement multithreading in the main program

--- spot_optimizer.py
+++ spot_optimizer.py
@@ -1,3 +1,5 @@
+import time
+
 from spot_optimizer_functions import *
 
 if __name__ == "__main__":
@@ -8,17 +10,26 @@ if __name__ == "__main__":
     desired_regions = ['us-west-2', 'eu-west-1', 'ap-southeast-1']
     # Global price list of the instances, which matches the requirements
     instances_prices = []
-    # Iterate over each region
+    # Initialize thread lock
+    lock = threading.Lock()
+    # Create and run a separate thread for each region
+    threads = []
     for region in desired_regions:
-        # Retrieve and add regional price list to the global list
-        instances_prices.extend(get_pricelist_regional(profile_name='spot_optimizer',
-                                                       region_name=region,
-                                                       vcpu_min=desired_vcpu,
-                                                       ram_min=desired_ram))
-
+        time.sleep(0.100)
+        thread = threading.Thread(target=thread_method, args=(get_pricelist_regional,
+                                                              {'profile_name': 'spot_optimizer',
+                                                               'region_name': region,
+                                                               'vcpu_min': desired_vcpu,
+                                                               'ram_min': desired_ram},
+                                                              lock,
+                                                              instances_prices))
+        thread.start()
+        threads.append(thread)
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
     # Sort instances by price
     sorted_prices_list = sorted(instances_prices, key=lambda x: x['Price'], reverse=False)
-
     # Print 5 cheapest instances that matched the provided requirements
     print("Top 5 cheapest instances that matched the provided requirements:")
     for instance_price in sorted_prices_list[:5]:
