e893f47655d6282a646c1c5cfba04f804d6679ac
Oleksandr Patuk <oleksandr.patuk@gmail.com>
2023-07-17 18:19:53 +0300

    Implement get_pricelist_regional funcion

--- spot_optimizer.py
+++ spot_optimizer.py
@@ -16,8 +16,69 @@ def thread_method_no_lock(function, function_args, results_list):
     results_list.extend(function_result)
 
 
+def get_pricelist_regional(profile_name='default',
+                           region_name=None,
+                           vcpu_min=None,
+                           vcpu_max=None,
+                           ram_min=None,
+                           ram_max=None):
+    # Calculate desired parameters if not set
+    if vcpu_min is None:
+        vcpu_min = 0
+    if vcpu_max is None:
+        vcpu_max = vcpu_min * 3
+    if ram_min is None:
+        ram_min = 0
+    if ram_max is None:
+        ram_max = ram_min * 3
+    # Initialize regional lock
+    lock_regional = threading.Lock()
+    # Initialize the AWS session
+    session = boto3.Session(profile_name=profile_name, region_name=region_name)
+    # Get list of available instances in the region, that matched the provided requirements
+    instances_list = get_matched_instances(session=session,
+                                           vcpu_min=vcpu_min,
+                                           vcpu_max=vcpu_max,
+                                           ram_min=ram_min,
+                                           ram_max=ram_max)
+    # Initialize regional price list
+    instances_prices_regional = []
+    # Create a thread and get prices for the matched on-demand  instances
+    thread_on_demand = threading.Thread(target=thread_method,
+                                        args=(get_ec2_on_demand_prices, {'session': session,
+                                                                         'region_code': region,
+                                                                         'instance_types': instances_list},
+                                              lock_regional,
+                                              instances_prices_regional))
+    thread_on_demand.start()
+    # Create a thread and get prices for the matched spot instances
+    thread_spot = threading.Thread(target=thread_method,
+                                   args=(get_spot_prices, {'session': session,
+                                                           'instances_types': instances_list},
+                                         lock_regional,
+                                         instances_prices_regional))
+    thread_spot.start()
+    # Initialise descriptions list
+    instances_description = []
+    # Create a thread and get descriptions for the matched instances
+    thread_descriptions = threading.Thread(target=thread_method_no_lock,
+                                           args=(get_instances_descriptions, {'session': session,
+                                                                              'instance_types': instances_list},
+                                                 instances_description))
+    thread_descriptions.start()
+    # Wait for the created threads finish
+    thread_on_demand.join()
+    thread_spot.join()
+    thread_descriptions.join()
+    # Add instances descriptions to the instances price-list
+    pricelist_add_descriptions(instances_pricelist=instances_prices_regional,
+                               instances_descriptions=instances_description,
+                               region=region)
+
+    return instances_prices_regional
+
+
 if __name__ == "__main__":
-    # Configure desired parameters of the instance
     # Set the required parameters
     desired_vcpu = 2
     desired_ram = 16
@@ -27,60 +88,11 @@ if __name__ == "__main__":
     instances_prices = []
     # Iterate over each region
     for region in desired_regions:
-        # Initialize regional lock
-        lock_regional = threading.Lock()
-
-        # Initialize the AWS session
-        session = boto3.Session(profile_name='spot_optimizer', region_name=region)
-
-        # Get list of available instances in the region, that matched the provided requirements
-        instances_list = get_matched_instances(session=session,
-                                               vcpu_min=desired_vcpu,
-                                               vcpu_max=desired_vcpu * 3,
-                                               ram_min=desired_ram,
-                                               ram_max=desired_ram * 3)
-
-        # Initialize regional price list
-        instances_prices_regional = []
-        # Create a thread and get prices for the matched on-demand  instances
-        thread_on_demand = threading.Thread(target=thread_method,
-                                            args=(get_ec2_on_demand_prices, {'session': session,
-                                                                             'region_code': region,
-                                                                             'instance_types': instances_list},
-                                                  lock_regional,
-                                                  instances_prices_regional))
-        thread_on_demand.start()
-
-        # Create a thread and get prices for the matched spot instances
-        thread_spot = threading.Thread(target=thread_method,
-                                       args=(get_spot_prices, {'session': session,
-                                                               'instances_types': instances_list},
-                                             lock_regional,
-                                             instances_prices_regional))
-        thread_spot.start()
-
-        # Initialise descriptions list
-        instances_description = []
-
-        # Create a thread and get descriptions for the matched instances
-        thread_descriptions = threading.Thread(target=thread_method_no_lock,
-                                               args=(get_instances_descriptions, {'session': session,
-                                                                                  'instance_types': instances_list},
-                                                     instances_description))
-        thread_descriptions.start()
-
-        # Wait for the created threads finish
-        thread_on_demand.join()
-        thread_spot.join()
-        thread_descriptions.join()
-
-        # Add instances descriptions to the instances price-list
-        pricelist_add_descriptions(instances_pricelist=instances_prices_regional,
-                                   instances_descriptions=instances_description,
-                                   region=region)
-
-        # Add regional price list to the global list
-        instances_prices.extend(instances_prices_regional)
+        # Retrieve and add regional price list to the global list
+        instances_prices.extend(get_pricelist_regional(profile_name='spot_optimizer',
+                                                       region_name=region,
+                                                       vcpu_min=desired_vcpu,
+                                                       ram_min=desired_ram))
 
     # Sort instances by price
     sorted_prices_list = sorted(instances_prices, key=lambda x: x['Price'], reverse=False)
