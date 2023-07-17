91349e02e6963702a055d24df903092dc9641489
Oleksandr Patuk <oleksandr.patuk@gmail.com>
2023-07-17 18:30:17 +0300

    Refactor code - move newly implemented functions to the spot_optimizer_functions.py file

--- spot_optimizer_functions.py
+++ spot_optimizer_functions.py
@@ -1,6 +1,8 @@
 import json
+import threading
 from datetime import datetime
 
+import boto3
 from pkg_resources import resource_filename
 
 
@@ -229,3 +231,76 @@ def pricelist_add_descriptions(instances_pricelist, instances_descriptions, regi
                 instance_price["Discount"] = 0
         else:
             instance_price["Discount"] = 0
+
+
+def thread_method(function, function_args, lock, results_list):
+    function_result = function(**function_args)
+    with lock:
+        results_list.extend(function_result)
+
+
+def thread_method_no_lock(function, function_args, results_list):
+    function_result = function(**function_args)
+    results_list.extend(function_result)
+
+
+def get_pricelist_regional(profile_name='default',
+                           region_name=None,
+                           vcpu_min=None,
+                           vcpu_max=None,
+                           ram_min=None,
+                           ram_max=None):
+    # Calculate desired parameters if not set
+    if vcpu_min is None:
+        vcpu_min = 0
+    if vcpu_max is None:
+        vcpu_max = vcpu_min * 3
+    if ram_min is None:
+        ram_min = 0
+    if ram_max is None:
+        ram_max = ram_min * 3
+    # Initialize regional lock
+    lock_regional = threading.Lock()
+    # Initialize the AWS session
+    session = boto3.Session(profile_name=profile_name, region_name=region_name)
+    # Get list of available instances in the region, that matched the provided requirements
+    instances_list = get_matched_instances(session=session,
+                                           vcpu_min=vcpu_min,
+                                           vcpu_max=vcpu_max,
+                                           ram_min=ram_min,
+                                           ram_max=ram_max)
+    # Initialize regional price list
+    instances_prices_regional = []
+    # Create a thread and get prices for the matched on-demand  instances
+    thread_on_demand = threading.Thread(target=thread_method,
+                                        args=(get_ec2_on_demand_prices, {'session': session,
+                                                                         'region_code': region_name,
+                                                                         'instance_types': instances_list},
+                                              lock_regional,
+                                              instances_prices_regional))
+    thread_on_demand.start()
+    # Create a thread and get prices for the matched spot instances
+    thread_spot = threading.Thread(target=thread_method,
+                                   args=(get_spot_prices, {'session': session,
+                                                           'instances_types': instances_list},
+                                         lock_regional,
+                                         instances_prices_regional))
+    thread_spot.start()
+    # Initialise descriptions list
+    instances_description = []
+    # Create a thread and get descriptions for the matched instances
+    thread_descriptions = threading.Thread(target=thread_method_no_lock,
+                                           args=(get_instances_descriptions, {'session': session,
+                                                                              'instance_types': instances_list},
+                                                 instances_description))
+    thread_descriptions.start()
+    # Wait for the created threads finish
+    thread_on_demand.join()
+    thread_spot.join()
+    thread_descriptions.join()
+    # Add instances descriptions to the instances price-list
+    pricelist_add_descriptions(instances_pricelist=instances_prices_regional,
+                               instances_descriptions=instances_description,
+                               region=region_name)
+
+    return instances_prices_regional
